c065402e300637806e3ab91c48f0c357e67733f8
Jacinto Aisa Ibañez <jacinto_aisa@hotmail.com>
2024-06-25 12:18:32 +0200

    Antes de Prueba

---
+++ Linkedin.DogMeasures/Services/ServicioParaProbar.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using Linkedin.DogMeasures.Models;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace Linkedin.DogMeasures.Services
+{
+    public class ServicioParaProbar
+    {
+
+        private readonly List<DogWeightInfo> _weightInfos;
+        public ServicioParaProbar()
+        {
+            _weightInfos = new List<DogWeightInfo>
+            {
+                new DogWeightInfo()
+                {
+                    Message = "Estas muy gordo", DeviationType = DogWeightInfo.WeightDeviationType.Overweight,
+                    WeightDeviation = 0.4M
+                },
+                new DogWeightInfo()
+                {
+                    Message = "Estas muy delgado", DeviationType = DogWeightInfo.WeightDeviationType.BelowWeight,
+                    WeightDeviation = 0.1M
+                },
+                new DogWeightInfo()
+                {
+                    Message = "Estas excelente", DeviationType = DogWeightInfo.WeightDeviationType.InRange,
+                    WeightDeviation = 0.05M
+                },
+            };
+
+
+        }
+
+        public decimal DameDesviacion(DogWeightInfo.WeightDeviationType tipo)
+        {
+            switch (tipo)
+            {
+                case DogWeightInfo.WeightDeviationType.InRange:
+                    return 0.5M;
+                case DogWeightInfo.WeightDeviationType.Overweight:
+                    return 0.8M;
+                case DogWeightInfo.WeightDeviationType.BelowWeight:
+                    return 1.0M;
+                default: return 0;
+            }
+            
+        }
+    }
+}
